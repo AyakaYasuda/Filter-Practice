0c4bf95c43e1e7e5bb213ef426cf6f278538e986
Ayaka Yasuda <ayakayasuda23@gmail.com>
2021-11-26 21:32:03 -0800

    created a filterable contact list

--- practice-2/main.js
+++ practice-2/main.js
@@ -1,2 +1,27 @@
 'use strict';
-console.log('hello');
+
+// Get input element
+let filterInput = document.getElementById('filterInput');
+// Add event listener
+filterInput.addEventListener('keyup', filterNames);
+
+function filterNames() {
+  // Get value of input
+  let filterValue = document.getElementById('filterInput').value.toUpperCase();
+
+  // Get names ul
+  let ul = document.getElementById('names');
+  // Get lis from ul
+  let li = ul.querySelectorAll('.collection-item');
+
+  // Loop through collection-item lis
+  for (let i = 0; i < li.length; i++) {
+    let a = li[i].getElementsByTagName('a')[0];
+    // If match
+    if (a.innerHTML.toUpperCase().indexOf(filterValue) > -1) {
+      li[i].style.display = '';
+    } else {
+      li[i].style.display = 'none';
+    }
+  }
+}
